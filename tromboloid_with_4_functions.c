27ba494ff4bdee8e44a5699033db1cab049fb2b7
ENG19CS0051-ASHWINI <76114306+ENG19CS0051-ASHWINI@users.noreply.github.com>
2021-02-13 10:56:31 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,26 @@
 //WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+int input ()
+{
+int a;
+printf("enter the number:\n");
+scanf("%d",&a);
+return a;
+}
+int find_sum(int a,int b)
+{
+return a+b;
+}
+void output (int a,int b, int c)
+{ 
+printf("sum of %d+%d is %d\n",a,b,c);
+}
+int main()
+{
+int x,y,z;
+x=input();
+y=input();
+z=find_sum(x,y);
+output(x,y,z);
+return 0;
+}
